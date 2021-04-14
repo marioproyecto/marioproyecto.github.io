30b37519c2479ee620ba100530e3c5e45f0b4583
marioproyecto <82174987+marioproyecto@users.noreply.github.com>
2021-04-13 23:49:54 -0500

    Update CtrlAlumnos.js

--- js/CtrlAlumnos.js
+++ js/CtrlAlumnos.js
@@ -68,6 +68,10 @@ function htmlFila(doc) {
   const data = doc.data();
   const matricula = cod(data.matricula);
   const nombre = cod(data.nombre);
+  var fsf= cod(data.fecha);
+  var fecha = new Date(fsf);
+  var espacio="[   -   ]";
+  var dformat = [fecha.getDate()+1, fecha.getMonth()+1, fecha.getFullYear()].join('/');
   const parámetros =
     new URLSearchParams();
   parámetros.append("id", doc.id);
@@ -76,9 +80,10 @@ function htmlFila(doc) {
       <a class="fila" href=
   "alumno.html?${parámetros}">
         <strong class="primario">
-          ${nombre}
+          ${matricula} ${nombre} ${dformat}
         </strong>
       </a>
+     
     </li>`);
 }
 
